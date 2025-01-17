82954491b5ce80b05cd24ac1e26bfd108fe16642
fred248h <frederik_e@live.com>
2025-01-17 14:38:38 +0100

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,6 @@
 #from mlops_groupp_66.data import MyDataset
 
-from mlops_groupp_66.data import MyDataset, get_nn_dataloaders, get_transformer_dataloaders
+#from mlops_groupp_66.data import MyDataset, get_nn_dataloaders, get_transformer_dataloaders
 
 def test_dummy():
     assert 1 + 1 == 2
