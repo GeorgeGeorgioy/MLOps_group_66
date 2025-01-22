553e60cadc87725d954fc7e32ae1c61fc017f66e
fred248h <frederik_e@live.com>
2025-01-22 10:50:47 +0100

    Update requirements

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,6 @@
 #from mlops_groupp_66.data import MyDataset
 
-from mlops_groupp_66.data import MyDataset, get_transformer_dataloaders, preprocess
+#from mlops_groupp_66.data import MyDataset, get_transformer_dataloaders, preprocess
 
 def test_dummy():
     assert 1 + 1 == 2
