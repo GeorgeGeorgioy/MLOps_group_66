2525b995f19b2287c971300f475153c85a697d4d
marconesdtu <s233557@dtu.dk>
2025-01-24 10:38:35 +0100

    final src

--- src/mlops_groupp_66/train.py
+++ src/mlops_groupp_66/train.py
@@ -4,7 +4,7 @@ from transformers import DistilBertTokenizer, DistilBertForSequenceClassificatio
 from model import FraudTransformer
 from pathlib import Path
 from dotenv import load_dotenv
-from .data import get_transformer_dataloaders
+from data import get_transformer_dataloaders
 import pandas as pd
 import os
 import wandb
--- src/mlops_groupp_66/train_hyp_sweep.py
+++ src/mlops_groupp_66/train_hyp_sweep.py
@@ -8,7 +8,7 @@ from transformers import AdamW, DistilBertTokenizer
 from transformers import DistilBertForSequenceClassification
 import torch
 import pytorch_lightning as pl
-from src.mlops_groupp_66.model import FraudTransformer
+from model import FraudTransformer
 from pathlib import Path
 from dotenv import load_dotenv
 from data import get_transformer_dataloaders
